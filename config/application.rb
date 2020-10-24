90383cbf84dd1e14139768b309f6b3a286f14a3e
dhs <hetre85@gmail.com>
2020-10-24 14:56:23 -0400

    Use rack-cors, update middleware to allow api call

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,14 @@ module RailsGraphql
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # config/application.rb
+    # CORS config to allow ajax
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
