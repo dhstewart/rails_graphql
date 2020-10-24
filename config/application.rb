943a3a2ee917b5271fd962894a98a2e18f0a57b3
dhs <hetre85@gmail.com>
2020-10-24 16:17:57 -0400

    Allow origins

--- config/application.rb
+++ config/application.rb
@@ -37,11 +37,11 @@ module RailsGraphql
     # config/application.rb
     # CORS config to allow ajax
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins '*'
-    #     resource '*', headers: :any, methods: [:get, :post, :options]
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
